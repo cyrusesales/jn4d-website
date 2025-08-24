fc6183f42508cd9c6244805132589e65234c13af
cyrusesales <cyrus.sales@outlook.com>
2025-08-24 22:51:03 +0800

    configured uploading image feature

--- jn4dwebsite/homepage/views.py
+++ jn4dwebsite/homepage/views.py
@@ -6,11 +6,14 @@ from .models import Header
 # Create your views here.
 
 
+def base(request):
+    headers = Header.objects.all()
+    context = {
+        'headers': headers
+    }
+    return render(request, 'base.html', context)
+
+
 def homepage(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
-
-
-def display_header_logo(request):
-    headers = Header.objects.all()
-    return render(request, 'base.html', {'header_objects': headers})
