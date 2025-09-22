33c32cc16e3fabeaf37cd9aea139d0629d7299c0
cyrusesales <cyrus.sales@outlook.com>
2025-09-22 23:50:20 +0800

    added product model

--- jn4dwebsite/homepage/models.py
+++ jn4dwebsite/homepage/models.py
@@ -28,3 +28,22 @@ class Category(models.Model):
 
     def __str__(self):
         return self.id
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.CharField(max_length=150, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to='products/')
+    description = models.CharField(max_length=200, blank=True, null=True)
+    quantity = models.IntegerField(null=True, blank=True)
+    original_price = models.FloatField(null=True, blank=True)
+    selling_price = models.FloatField(null=True, blank=True)
+    status = models.BooleanField(default=False, help_text="0=default, 1=Hidden")
+    trending = models.BooleanField(default=False, help_text="0=default, 1=Trending")
+    tag = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
+    
