b5b0c3c73c685074772a5eb0c863ad1774599151
cyrusesales <cyrus.sales@outlook.com>
2026-01-05 09:28:15 +0800

    added USE_L10N = True and USE_THOUSAND_SEPARATOR = True in settings.py

--- jn4dwebsite/jn4dwebsite/settings.py
+++ jn4dwebsite/jn4dwebsite/settings.py
@@ -135,3 +135,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USE_L10N = True  # Often included with USE_I18N=True and others
+USE_THOUSAND_SEPARATOR = True
